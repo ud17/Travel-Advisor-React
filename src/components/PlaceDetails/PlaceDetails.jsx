623c79d219241d5a736d00835ca2996f33085a0e
ud17 <upandya1297@gmail.com>
2022-03-30 14:43:35 -0400

    card details completed

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -18,7 +18,42 @@ const PlaceDetails = ({place}) => {
                 title={ place.name }
             />
             <CardContent>
-                <Typography gutterBottom variant="3">{place.name}</Typography>
+                <Typography gutterBottom variant="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name} />
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({ name }) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip}/>
+                ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" className={classes.subtitle} color="textSecondary">
+                        <LocationOnIcon /> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" className={classes.spacing} color="textSecondary">
+                        <PhoneIcon /> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={()=> {window.open(place.web_url, "_blank")}}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={()=> {window.open(place.website, "_blank")}}>
+                        Website
+                    </Button>
+                </CardActions>
             </CardContent>
         </Card>
     );
